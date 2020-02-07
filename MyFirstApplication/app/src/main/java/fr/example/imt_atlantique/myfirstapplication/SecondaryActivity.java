bd4dc474e6aa95c7118fbca1b801d89780407d56
Victor Gambier <victor.gambier@imt-atlantique.net>
2020-02-07 17:27:04 +0100

    Initial commit

---
+++ MyFirstApplication/app/src/main/java/fr/example/imt_atlantique/myfirstapplication/SecondaryActivity.java
@@ -0,0 +1,70 @@
+package fr.example.imt_atlantique.myfirstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class SecondaryActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondary);
+        Log.i("Lifecycle", "onCreate method");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Lifecycle", "onStart method");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("Lifecycle", "onRestart method");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Lifecycle", "onResume method");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Lifecycle", "onPause method");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i("Lifecycle", "onStop method");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i("Lifecycle", "onDestroy method");
+    }
+
+    public void validateAction (View v) {
+
+        EditText firstname = findViewById(R.id.editTextfirstName);
+        String textToShow = firstname.getText().toString();
+
+        Toast.makeText(getApplicationContext(), textToShow, Toast.LENGTH_LONG).show();
+        Snackbar.make(v, textToShow, Snackbar.LENGTH_LONG).show();
+
+        Log.i("Message", "Button method "+textToShow);
+
+
+    }
+
+}
---
+++ MySecondApplication/app/src/main/java/fr/example/imt_atlantique/mysecondapplication/ui/share/ShareFragment.java
@@ -0,0 +1,35 @@
+package fr.example.imt_atlantique.mysecondapplication.ui.share;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import fr.example.imt_atlantique.mysecondapplication.R;
+
+public class ShareFragment extends Fragment {
+
+    private ShareViewModel shareViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        shareViewModel =
+                ViewModelProviders.of(this).get(ShareViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_share, container, false);
+        final TextView textView = root.findViewById(R.id.text_share);
+        shareViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
